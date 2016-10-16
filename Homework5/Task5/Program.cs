300b03cadaf8e49bf86a11e671c306a7fba06b80
spasimiram <spasimiram@gmail.com>
2016-10-16 14:04:29 +0300

    homework 4/5

--- Homework5/Task5/Program.cs
+++ Homework5/Task5/Program.cs
@@ -4,37 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task4
+namespace Task5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of columns: ");
+            int columns = int.Parse(Console.ReadLine());
 
-            int[,] matrix = new int[5, 6];
-            int number = 31;
+            int[,] matrix = new int[columns, rows];
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    
-                        number--;
-                        matrix[i, j] = number;
-                    
+                    string a = string.Format("Enter element for {0}:{1}", j, i);
+                    Console.WriteLine(a);
+                    matrix[j, i] = int.Parse(Console.ReadLine());
                 }
             }
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    Console.Write("|{0}| \t", matrix[i, j]);
+                    Console.Write("|" + matrix[i, j] + "|" + "\t");
 
                 }
                 Console.WriteLine();
             }
         }
-
-
     }
 }
-
